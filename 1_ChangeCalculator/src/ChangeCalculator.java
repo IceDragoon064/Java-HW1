bdfe802ad27d4567398e4c77b73426cf709f2b61
Leon Lu <ltlu1993@gmail.com>
2022-04-30 21:25:50 -0400

    Finalized ChangeCalculator.java

--- 1_ChangeCalculator/src/ChangeCalculator.java
+++ 1_ChangeCalculator/src/ChangeCalculator.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
-/**
- * @author Leon Lu
- * Project Due Date: May 3, 2022 (Tuesday)
- */
-
 
 /**
  * This is HW1, Problem 1
  * @author Leon Lu
+ * Project Due Date: May 3, 2022 (Tuesday)
  * This class calculate which bill or coin is needed 
  * based on user money input 
  */
@@ -17,14 +13,14 @@ public class ChangeCalculator
 	
 	// Function to calculate how many
 	// Bill and/or coin will be used
-	static void calculateValue(int changeAmount) 
+	static void calculateValue(Double changeAmount) 
 	{
 	    // Variable as a remainder from subtracting changeAmount with the above variables
-	    int remainingAmount = changeAmount;	
+	    Double remainingAmount = changeAmount;	
 	    
-	    // Array to store money value as float
-	    int[] moneyValue = 
-	    	{2000, 1000, 500, 100, 25, 10, 5, 1};
+	    // Array to store money value as double
+	    Double[] moneyValue = 
+	    	{20.00, 10.00, 5.00, 1.00, 0.25, 0.10, 0.05, 0.01};
 	    
 	    // Array to store string println output
 	    // Each element must match with moneyValue element
@@ -37,13 +33,28 @@ public class ChangeCalculator
 	    {
 	    	// Variable to show how many bills or coins will be needed
 	    	int value = 0;
+	    	Boolean elementDone = false;
 	    	
-	    	if(remainingAmount >= moneyValue[i])
+	    	// While loop will continue until remainingAmount is less than moneyValue
+	    	// Example: if remainingAmount is 19.81 and moneyValue is 20.00, 
+	    	// the while loop will be stopped by elementDone boolean setting to true
+	    	while(!elementDone)
 	    	{
-	    		value = remainingAmount / moneyValue[i];
-	    		remainingAmount = remainingAmount - (value * moneyValue[i]);
+		    	if(remainingAmount >= moneyValue[i])
+		    	{
+		    		value++;
+		    		remainingAmount = remainingAmount - moneyValue[i];
+		    		
+		    		//Ensure double is properly rounded up
+		    		remainingAmount = Math.round(remainingAmount * 100.0) / 100.0;
+		    	}
+		    	
+		    	else
+		    	{
+		    		elementDone = true;
+		    	}
 	    	}
-	    	
+
 	    	// Print out how many bill or coin
 	    	System.out.println(value + outputText[i]);
 	    }  
@@ -52,23 +63,21 @@ public class ChangeCalculator
 
 	public static void main(String args[])
 	{  
-		Integer changeAmount  = 0;
+		Double changeAmount  = 0.0;
 		Boolean correctInput = false;
 		//While loop to ensure that the user inputs a float before continuing
 		while(!correctInput)
 		{
 			// Create scanner object
 			Scanner userInput = new Scanner(System.in);  
-		    System.out.println("Enter change amount without the $ symbol and as an integer. Example: $13.36 will be 1336");
+		    System.out.println("Enter change amount without the $ symbol and as a double. Example: $13.36 will be 13.36");
 
 		    
-		    // Check if input is an integer
-		    // Notify user when an integer is found instead of double
-		    if(userInput.hasNextInt())
+		    // Check if input is a double
+		    if(userInput.hasNextDouble())
 		    {
-		    	changeAmount = userInput.nextInt();
-		    	float printValue = changeAmount.floatValue() / 100;
-		    	System.out.println("Amount: $" + String.format("%.2f", printValue));
+		    	changeAmount = userInput.nextDouble();
+		    	System.out.println("Amount: $" + String.format("%.2f", changeAmount));
 		    	correctInput = true;
 		    }
 
