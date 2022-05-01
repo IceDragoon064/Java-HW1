79c0039b05d3afe58e1991e9f8b19b5f9a1ab1e0
Leon Lu <ltlu1993@gmail.com>
2022-04-30 22:19:48 -0400

    Finalize two java files

---
+++ 2_Cube/src/CubeDriver.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * This is HW1, problem 3
+ * @author Leon Lu
+ * Project Due Date: May 3, 2022 (Tuesday)
+ * This class will test CubeIt.java class and methods
+ */
+public class CubeDriver 
+{
+	public static void main(String args[])
+	{
+		// Initialize variables
+		double sideLength = 0.0;
+		
+		// Variable used for while loop parameter
+		boolean correctInput = false;
+		
+		// Get user input
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Input cube side length");
+		
+		// Force user to input a double before proceeding with the program
+		while(!correctInput)
+		{
+			// Check input as a double
+			// If the input is not a double, print a warning
+			
+			if (userInput.hasNextDouble())
+			{
+				sideLength = userInput.nextDouble();
+				System.out.println("Length: " + sideLength);
+				correctInput = true;
+			}
+			
+			else
+			{
+				System.out.println("Wrong input. Try again.");
+			}
+		}
+		
+		// Use CubeIt class constructor to create new CubeIt object and assign its variable
+		CubeIt cubeValue = new CubeIt(sideLength);
+		
+		System.out.println("Surface Area: " + cubeValue.surfaceArea());
+		System.out.println("Volume: " + cubeValue.volume());
+	}
+}
---
+++ 2_Cube/src/CubeIt.java
@@ -0,0 +1,45 @@
+
+/**
+ * This is HW1, Problem 2
+ * @author Leon Lu
+ * Project Due Date: May 3, 2022 (Tuesday)
+ * This class will calculate surface area and volume
+ */
+
+public class CubeIt
+{
+	private double sideLength;
+	
+	/**
+	 * Constructor 
+	 * @param sideLength
+	 */
+	public CubeIt(double sideLength)
+	{
+		this.sideLength = sideLength;
+	}
+	
+	/**
+	 * surfaceArea method to calculate surface area of the cube
+	 * Formula: SA = 6 * side length ^ 2
+	 * @return the value of saCube using instance variable sideLength
+	 */
+	public double surfaceArea()
+	{
+		double saCube = 6 * Math.pow(this.sideLength,2);
+		return saCube;
+
+	}
+	
+	/**
+	 * This method calculates the volume of the cube
+	 * using instance variable sideLength
+	 * Formula: V = side length ^ 3 
+	 * @return the value of volumeCube
+	 */
+	public double volume()
+	{
+		double volumeCube = Math.pow(this.sideLength,3);
+		return volumeCube;
+	}
+}
